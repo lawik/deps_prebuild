bd95502036597c647113a47a669ecc5978c858b5
Lars Wikman <lars@underjord.io>
2024-06-26 12:05:34 +0200

    hack poc unpack

--- lib/deps_prebuild.ex
+++ lib/deps_prebuild.ex
@@ -5,16 +5,54 @@ defmodule DepsPrebuild do
     |> Path.wildcard()
     |> Enum.map(&to_charlist/1)
     |> then(fn filenames -> 
-      :erl_tar.create("#{archive_name}.tar.gz", filenames, compressed: true)
+      :erl_tar.create("#{archive_name}.tar.gz", filenames, [:compressed])
     end)
   end
 
   def unpack(archive_path, new_dir) do
     File.mkdir_p!(new_dir)
 
-    :erl_tar.extract(archive_path, cwd: new_dir, compressed: true)
+    :erl_tar.extract(archive_path, [{:cwd, new_dir}, :compressed])
+    |> dbg()
+  end
+
+  def search(search, page, sort \\ "recent_downloads") do
+    query_string = :hex_api.encode_query_string(search: search, page: page, sort: sort)
+    config = :hex_core.default_config()
+
+    path =
+      config
+      |> :hex_api.build_repository_path(["packages"])
+      |> :hex_api.join_path_segments()
+
+    url = <<path::binary, "?", query_string::binary>>
+    case :hex_api.get(config, url) do
+      {:ok, {_status, _headers, body}} -> {:ok, body}
+      {:error, reason} -> {:error, reason}
+    end
+  end
+
+  def download(package, version) do
+    config = :hex_core.default_config()
+    case :hex_repo.get_tarball(config, package, version) do
+      {:ok, {200, _, tarball}} -> {:ok, tarball}
+      {:error, reason} -> {:error, reason}
+    end
+    
+
   end
   
+  
+
+#   search(Config, Query, SearchParams) when
+#     is_map(Config) and is_binary(Query) and is_list(SearchParams)
+# ->
+#     QueryString = hex_api:encode_query_string([{search, Query} | SearchParams]),
+#     Path = hex_api:join_path_segments(hex_api:build_repository_path(Config, ["packages"])),
+#     PathQuery = <<Path/binary, "?", QueryString/binary>>,
+#     hex_api:get(Config, PathQuery).
+  
+
   
   # def tar(dir) do
   #   dir
--- test/deps_prebuild_test.exs
+++ test/deps_prebuild_test.exs
@@ -12,6 +12,42 @@ defmodule DepsPrebuildTest do
 
     DepsPrebuild.unpack(target_file, out_dir)
   end
+
+  @tag :tmp_dir
+  test "fetch hex package", %{tmp_dir: tmp_dir} do
+    result = DepsPrebuild.search("", 1)
+    assert {:ok, stuff} = result
+    
+    stuff
+    |> Enum.map(fn pkg ->
+      assert %{"name" => name, "latest_stable_version" => version} = pkg
+
+      assert {:ok, tarball} = DepsPrebuild.download(name, version)
+      pkg_path = Path.join(tmp_dir, "pkg.tgz")
+      unpack_path = Path.join(tmp_dir, "pkg")
+      File.mkdir_p!(unpack_path)
+      File.write!(pkg_path, tarball)
+
+      assert :ok = DepsPrebuild.unpack(pkg_path, unpack_path)
+      dbg(File.ls!(unpack_path) |> Enum.map(& {&1, File.stat!(Path.join(unpack_path, &1))}))
+      #dbg(File.read!(Path.join(unpack_path, "metadata.config")))
+
+      contents_path = Path.join(unpack_path, "contents")
+      File.mkdir_p!(contents_path)
+
+      assert :ok = DepsPrebuild.unpack(Path.join(unpack_path, "contents.tar.gz"), contents_path)
+      dbg(contents_path)
+      dbg(File.ls!(unpack_path))
+      dbg(File.ls!(contents_path))
+
+      mix_dir = contents_path
+      IO.puts("Getting deps for #{name}")
+      assert {_, 0} = System.shell("mix deps.get", cd: mix_dir)
+      IO.puts("Compiling #{name}")
+      assert {_, 0} = System.shell("mix compile", cd: mix_dir)
+    end)
+  end
+  
   
   # @tag :tmp_dir
   # test "compress and decompress file streaming", %{tmp_dir: tmp_dir} do
