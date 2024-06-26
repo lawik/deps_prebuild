f98b72bc3b7d6ba100cfc8c8658739ef348a5e93
Lars Wikman <lars@underjord.io>
2024-06-26 11:08:11 +0200

    Trying hex_core

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule DepsPrebuild.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :hex]
+      extra_applications: [:logger, :inets, :crypto, :public_key]
     ]
   end
 
@@ -23,9 +23,10 @@ defmodule DepsPrebuild.MixProject do
     [
       {:jason, "~> 1.4"},
       {:stream_gzip, "~> 0.4.2"},
-      {:hex, github: "hexpm/hex", tag: "v2.1.1"},
-      #{:libzstd, "~> 1.3.7", github: "facebook/zstd", app: false},
-      #{:ex_zstd, "~> 0.1.0"}
+      # {:hex, github: "hexpm/hex", tag: "v2.1.1"},
+      {:hex_core, "~> 0.10.2"}
+      # {:libzstd, "~> 1.3.7", github: "facebook/zstd", app: false},
+      # {:ex_zstd, "~> 0.1.0"}
     ]
   end
 end
